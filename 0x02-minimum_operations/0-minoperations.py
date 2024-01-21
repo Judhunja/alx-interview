616aee5592b63ff3be4286db22b75ab6cac6a214
Judhunja <judxhunja@gmail.com>
2024-01-21 23:49:03 +0300

    minimum number of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ This module contains a function minOperations. """
-import math
 
 
 def minOperations(n: int) -> int:
@@ -12,9 +11,21 @@ def minOperations(n: int) -> int:
     This method calculates the fewest number of operations needed to
     result in exactly n H characters in the file.
     """
-    if n % 2 == 0 and n % 3 != 0:
-        return math.floor(math.sqrt(n)) + 2
-    elif n % 2 == 0:
-        return math.floor(math.sqrt(n)) + 4
-    else:
-        return math.floor(math.sqrt(n)) + 3
+    if n == 1:
+        return 0
+
+    min_ops = 0
+    length = 1
+    copy_all = 0
+
+    while length < n:
+        # it is necessary to perform Copy all
+        # if n is divisible by length
+        if (n % length) == 0:
+            # length is the number of 'h' characters in the file
+            copy_all = length
+            min_ops += 1
+        length += copy_all
+        min_ops += 1
+
+    return min_ops
