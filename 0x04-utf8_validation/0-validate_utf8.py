9f1221a52c51efe032391b669bce6d066e6a5e55
Judhunja <judxhunja@gmail.com>
2024-05-10 18:18:35 +0300

    checks if data is in valid utf-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 """This module contains a function validUTF8"""
 
-import codecs
+from typing import List
 
 
-def validUTF8(data):
+def validUTF8(data: List[int]) -> bool:
     """Checks whether a data set represents a valid
     utf-8 encoding"""
-    try:
-        codecs.decode(bytes(data), 'utf-8')
-        return True
-    except Exception:
-        return False
+    num_next = 0
+    for byte in data:
+        if num_next == 0:
+            if byte >> 7 == 0b0:  # single byte character
+                continue
+            elif byte >> 5 == 0b110:  # two byte character
+                num_next = 1
+            elif byte >> 4 == 0b1110:  # three byte character
+                num_next = 2
+            elif byte >> 3 == 0b11110:  # four byte character
+                num_next = 3
+        else:
+            if byte >> 6 != 0b10:
+                return False  # not a continuation byte
+            num_next -= 1
+    return num_next == 0
