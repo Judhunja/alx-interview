92b4e288f6a3e585d4c6db3123fe32f8e8b3d54b
Judhunja <judxhunja@gmail.com>
2024-07-26 03:22:58 +0300

    change the max for the dp array to total + 1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ This module contains a function makeChange """
-import math
 
 
 def makeChange(coins, total):
@@ -8,14 +7,14 @@ def makeChange(coins, total):
     get a given amount """
     if total <= 0:
         return 0
-    dp = [math.inf] * (total + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for a in range(1, total + 1):
         for c in coins:
             if a - c >= 0:
                 dp[a] = min(dp[a], 1 + dp[a - c])
-    if dp[total] != math.inf:
+    if dp[total] != total + 1:
         return dp[total]
     else:
         return -1
